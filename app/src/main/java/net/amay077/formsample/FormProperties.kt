d474da6606646ac1ae860c0e5f0b3a2f2037a8c5
amay077 <okuokuoku@gmail.com>
2017-12-08 18:01:15 +0900

    Update RxProperty to v4.0.0

--- app/src/main/java/net/amay077/formsample/FormProperties.kt
+++ app/src/main/java/net/amay077/formsample/FormProperties.kt
@@ -5,7 +5,7 @@ import android.arch.lifecycle.MutableLiveData
 import io.reactivex.Observable
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.rxkotlin.addTo
-import jp.keita.kagurazaka.rxproperty.Nothing
+import jp.keita.kagurazaka.rxproperty.NoParameter
 import jp.keita.kagurazaka.rxproperty.RxProperty
 import jp.keita.kagurazaka.rxproperty.toReadOnlyRxProperty
 import jp.keita.kagurazaka.rxproperty.toRxCommand
@@ -24,14 +24,14 @@ class FormProperties {
 
     /** ニックネーム */
     val nickname = RxProperty<String>("")
-            .setValidatorKt {
+            .setValidator {
                 if (it.length < 2 || it.length > 10)
                     // エラーの場合はその説明を、エラーなしの場合は null を返却
                     "ニックネームは2文字以上10文字以下にしてください" else null }
 
     /** 誕生日(Rawデータ) */
     val birthday = RxProperty<Calendar>(Calendar.getInstance())
-            .setValidatorKt {
+            .setValidator {
                 if (it >= Calendar.getInstance().apply { add(Calendar.YEAR, -18 ) }) "18歳以上が必要です" else null
             }
 
@@ -42,7 +42,7 @@ class FormProperties {
 
     /** 性別(Rawデータ) */
     val gender = RxProperty<Gender>(Gender.NOT_SET)
-            .setValidatorKt { if (it == Gender.NOT_SET) "性別を何か選択してください" else null }
+            .setValidator { if (it == Gender.NOT_SET) "性別を何か選択してください" else null }
 
     /** 性別(表示用文字列) */
     val genderTextResId = gender.map {
@@ -62,7 +62,7 @@ class FormProperties {
     val toast : LiveData<String> = _toast
 
     /** 登録ボタンが実行できるか */
-    private val canRegisterExecute : Observable<Boolean> = Observable
+    private val canRegistration: Observable<Boolean> = Observable
             .combineLatest(listOf(
                     nickname.onHasErrorsChanged().map { !it },
                     gender.onHasErrorsChanged().map { !it },
@@ -71,8 +71,8 @@ class FormProperties {
                     { anyList -> anyList.map { it as Boolean }.all { it }})
 
     /** 登録ボタンを押したときのコマンド */
-    // canRegisterExecute が true の時だけ実行可能なコマンド
-    val register = canRegisterExecute.toRxCommand<Nothing>()
+    // canRegistration が true の時だけ実行可能なコマンド
+    val register = canRegistration.toRxCommand<NoParameter>()
             .apply { this.subscribe {
                 // RxCommand の subscribe が呼ばれた時 = ボタンが押された時
                 // とりあえずトースト投げる
